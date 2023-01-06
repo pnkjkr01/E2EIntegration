443988d2f0aabcc2158cf510c10e9d527608be8b
2122539 <2122539@LTIN202225.cts.com>
2023-01-06 11:33:35 +0530

    Created automation project

---
+++ E2ESelneiumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("Hello");
+		driver.quit();
+	}
+
+}
